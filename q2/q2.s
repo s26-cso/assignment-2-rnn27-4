fa1bc80035d58836f1721e4ffcadc751bf1ccfd9
Nawander Raghavendra Navneet <raghavendra.nawander@students.iiit.ac.in>
2026-04-16 23:32:37 +0530

    One_Last

--- q2/q2.s
+++ q2/q2.s
@@ -1,129 +1,133 @@
+.data
+    space_fmt: .string "%d "
+    last_fmt: .string "%d\n"
+    newline_fmt: .string "\n"
+
 .text
 .globl main
-.extern printf
-.extern atoi
-.extern putchar
-
-.data
-fmt: .string "%d"     
-space: .string " "
-
-.bss
-arr: .space 8000
-res: .space 8000
-stack: .space 8000
-
-.text
 main:
-    addi sp, sp, -48
-    sd ra, 40(sp)
-    sd s0, 32(sp)
-    sd s1, 24(sp)
-    sd s2, 16(sp)
-    sd s3, 8(sp)
-
-    mv s0, a0         
-    mv s1, a1          
-
-    li s2, 1          
-    li s3, 0          
-
-reading:
-    bge s2, s0, read_completed
-    slli t0, s2, 3
-    add t1, s1, t0
-    ld a0, 0(t1)
+    #using s means we have to also save it...
+    addi sp, sp, -64
+    sd ra, 0(sp)
+    sd s0, 8(sp)
+    sd s1, 16(sp)
+    sd s2, 24(sp)
+    sd s3, 32(sp)
+    sd s4, 40(sp)
+    sd s5, 48(sp)
+    sd s6, 56(sp)
+    addi s0, a0, -1    #-1 cause the count also include the call to the executable file
+    ble s0, zero, the_end    #just in case there is nothing there forced by ai
+    #storing argc as we calling multiple functions we migth loose it other wise
+    mv s6, a1
+    #mallocing for storing numbers,result&stack
+    mv a0, s0
+    slli a0, a0, 3
+    call malloc
+    mv s1, a0
+    mv a0, s0
+    slli a0, a0, 3
+    call malloc
+    mv s2, a0
+    mv a0, s0
+    slli a0, a0, 3
+    call malloc
+    #redoing a0 cause its getting erased everytime also storing it ofc
+    mv s4, a0
+    #s4 now stores actual shite intead of argv
+    #now we want load up s3 we get argv
+    li s3, 0
+conversion:
+    #calling atoi to convert
+    bge s3, s0, reset
+    addi t0, s3, 1
+    slli t0, t0, 3
+    add t0, s6, t0
+    ld a0, 0(t0)
     call atoi
-    
-    la t3, arr
-    slli t2, s3, 3
-    add t4, t3, t2
-    sd a0, 0(t4)
-    
+    slli t1, s3, 3
+    add t1, s1, t1
+    sd a0, 0(t1)
+    li t2, -1    #initialising results to -1...
+    slli t1, s3, 3
+    add t1, s2, t1
+    sd t2, 0(t1)
     addi s3, s3, 1
-    addi s2, s2, 1
-    j reading
-
-read_completed:
-    addi t0, s3, -1    
-    li t1, -1        
-
-whynot:
-    blt t0, zero, print
-
-while:
-    blt t1, zero, next 
-    
-    la t2, stack
-    slli t3, t1, 3
-    add t4, t2, t3
-    ld t5, 0(t4)      
-
-    la t2, arr
-    slli t3, t5, 3
-    add t4, t2, t3
-    ld t5, 0(t4)      
-
-    la t2, arr
-    slli t3, t0, 3
-    add t4, t2, t3
-    ld t6, 0(t4)      
-
-    blt t6, t5, next   
-    addi t1, t1, -1    
-    j while
-
-next:
-    la t2, res
-    slli t3, t0, 3
-    add t4, t2, t3
-
-    blt t1, zero, negative
-    la t2, stack
-    slli t3, t1, 3
-    add t5, t2, t3
-    ld t6, 0(t5)
-    sd t6, 0(t4)     
-    j push_stack
-
-negative:
-    li t5, -1
-    sd t5, 0(t4)
-
-push_stack:
-    addi t1, t1, 1
-    la t2, stack
-    slli t3, t1, 3
-    add t4, t2, t3
-    sd t0, 0(t4)       
-    addi t0, t0, -1
-    j whynot
-
+    #would it have been smarter to just make a contant 8 and then use it everywhere?
+    #yess, but i am not smart enuf for that
+    j conversion
+reset:
+    li s5, -1
+    addi s3, s0, -1
+    blt s3, zero, print
+yet:
+    #so if nothing is there we put in if not we have conditions
+    blt s5, zero, end_condition
+    slli t0, s5, 3
+    add t0, s4, t0
+    ld t1, 0(t0)
+    slli t2, t1, 3
+    add t2, s1, t2
+    ld t2, 0(t2)
+    slli t3, s3, 3
+    add t3, s1, t3
+    ld t3, 0(t3)    #all this to get arr[stack.top()] and arr[i]
+    #now if arr[stack.top()] > arr[i] stop popping
+    bgt t2, t3, result
+    #yeetin
+    addi s5, s5, -1
+    j yet
+result:
+    #now we will have to update the result
+    slli t0, s3, 3
+    add t0, s2, t0
+    sd t1, 0(t0)
+end_condition:
+    ble s3, zero, print
+    addi s5, s5, 1
+    slli t0, s5, 3
+    add t0, s4, t0
+    sd s3, 0(t0)
+    addi s3, s3, -1
+    j yet
 print:
-    li s2, 0
-printing:
-    bge s2, s3, end
+    li s3, 0
+looping:
+    bge s3, s0, the_end
+    slli t1, s3, 3
+    add t1, s2, t1
+    ld a1, 0(t1)
 
-    beqz s2, skip_space
-    la a0, space
+    # Check if this is the last element
+    addi t2, s0, -1
+    beq s3, t2, print_last
+
+    la a0, space_fmt
     call printf
-skip_space:
-    la t0, res
-    slli t1, s2, 3
-    add t2, t0, t1
-    ld a1, 0(t2)
-    la a0, fmt
+    addi s3, s3, 1
+    j looping
+
+print_last:
+    # Print last element with newline, then restore
+    la a0, last_fmt
+    call printf
+    j restore_peace
+
+the_end:
+    #for the newline grandmasti (only runs if array was empty)
+    la a0, newline_fmt
     call printf
 
-    addi s2, s2, 1
-    j printing
-
-end:
+restore_peace:
+    #restoring peace
+    ld ra, 0(sp)
+    ld s0, 8(sp)
+    ld s1, 16(sp)
+    ld s2, 24(sp)
+    ld s3, 32(sp)
+    ld s4, 40(sp)
+    ld s5, 48(sp)
+    ld s6, 56(sp)
+    addi sp, sp, 64
     li a0, 0
-    ld ra, 40(sp)
-    ld s0, 32(sp)
-    ld s1, 24(sp)
-    ld s2, 16(sp)
-    ld s3, 8(sp)
-    addi sp, sp, 48
     ret
